03ded0fc1eb546bb8928b99eb96ce556db2813dd
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-05-10 15:10:32 +0100

    network tab interface

--- resources/js/components/phallery/dashboard/network.jsx
+++ resources/js/components/phallery/dashboard/network.jsx
@@ -8,6 +8,134 @@ class Network extends Component {
             <React.Fragment>
                 <Nav />
                 <Banner text="My Network" />
+                <div className="container my-3">
+                    <form className="form-inline ">
+                        <div className="col-md-6 mx-auto">
+                            <div className="input-group">
+                                <input
+                                    type="text"
+                                    className="form-control rounded-left"
+                                    placeholder="Search for friends"
+                                />
+                                <div className="input-group-prepend">
+                                    <button className="btn btn-success rounded-right">
+                                        <span className="fa fa-search text-white"></span>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+                    <div className="rounded-lg bg-primary text-white col-md-1  py-1 mx-auto text-center my-2 shadow-lg">
+                        <h4 className="font-weight-bold">55</h4>
+                        <small className="font-weight-bold">Connections</small>
+                    </div>
+
+                    <div className="row justify-content-around text-center mt-2 link-card">
+                        <a className="col-md-3 rounded-lg bg-white shadow">
+                            <p className="mt-2 fa-2x">
+                                <span className="fa fa-user-circle fa-2x "></span>
+                            </p>
+                            <p className="font-weight-bold">Adinoyi, Abraham</p>
+                            <p>
+                                <span className="fas fa-link"></span> 43
+                                connections
+                            </p>
+                        </a>
+
+                        <a className="col-md-3 rounded-lg bg-white shadow">
+                            <p className="mt-2 fa-2x">
+                                <span className="fa fa-user-circle fa-2x "></span>
+                            </p>
+                            <p className="font-weight-bold">Adinoyi, Abraham</p>
+                            <p>
+                                <span className="fas fa-link"></span> 43
+                                connections
+                            </p>
+                        </a>
+
+                        <a className="col-md-3 rounded-lg bg-white shadow">
+                            <p className="mt-2 fa-2x">
+                                <span className="fa fa-user-circle fa-2x "></span>
+                            </p>
+                            <p className="font-weight-bold">Adinoyi, Abraham</p>
+                            <p>
+                                <span className="fas fa-link"></span> 43
+                                connections
+                            </p>
+                        </a>
+                    </div>
+
+                    <div className="row justify-content-around text-center mt-2 link-card">
+                        <a className="col-md-3 rounded-lg bg-white shadow">
+                            <p className="mt-2 fa-2x">
+                                <span className="fa fa-user-circle fa-2x "></span>
+                            </p>
+                            <p className="font-weight-bold">Adinoyi, Abraham</p>
+                            <p>
+                                <span className="fas fa-link"></span> 43
+                                connections
+                            </p>
+                        </a>
+
+                        <a className="col-md-3 rounded-lg bg-white shadow">
+                            <p className="mt-2 fa-2x">
+                                <span className="fa fa-user-circle fa-2x "></span>
+                            </p>
+                            <p className="font-weight-bold">Adinoyi, Abraham</p>
+                            <p>
+                                <span className="fas fa-link"></span> 43
+                                connections
+                            </p>
+                        </a>
+
+                        <a className="col-md-3 rounded-lg bg-white shadow">
+                            <p className="mt-2 fa-2x">
+                                <span className="fa fa-user-circle fa-2x "></span>
+                            </p>
+                            <p className="font-weight-bold">Adinoyi, Abraham</p>
+                            <p>
+                                <span className="fas fa-link"></span> 43
+                                connections
+                            </p>
+                        </a>
+                    </div>
+
+                    <div className="row justify-content-around text-center mt-2 link-card">
+                        <a className="col-md-3 rounded-lg bg-white shadow">
+                            <p className="mt-2 fa-2x">
+                                <span className="fa fa-user-circle fa-2x "></span>
+                            </p>
+                            <p className="font-weight-bold">Adinoyi, Abraham</p>
+                            <p>
+                                <span className="fas fa-link"></span> 43
+                                connections
+                            </p>
+                        </a>
+
+                        <a className="col-md-3 rounded-lg bg-white shadow">
+                            <p className="mt-2 fa-2x">
+                                <span className="fa fa-user-circle fa-2x "></span>
+                            </p>
+                            <p className="font-weight-bold">Adinoyi, Abraham</p>
+                            <p>
+                                <span className="fas fa-link"></span> 43
+                                connections
+                            </p>
+                        </a>
+
+                        <a className="col-md-3 rounded-lg bg-white shadow">
+                            <p className="mt-2 fa-2x">
+                                <span className="fa fa-user-circle fa-2x "></span>
+                            </p>
+                            <p className="font-weight-bold">Adinoyi, Abraham</p>
+                            <p>
+                                <span className="fas fa-link"></span> 43
+                                connections
+                            </p>
+                        </a>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
--- resources/js/components/phallery/dashboard/notifications.jsx
+++ resources/js/components/phallery/dashboard/notifications.jsx
@@ -9,10 +9,10 @@ class Notifications extends Component {
                 <Banner text="Notifications" />
 
                 <div className="container my-3">
-                    <div className="text-center bg-white">
-                        <button className="btn btn-default">
+                    <div className="text-center bg-white py-2">
+                        <button className="btn btn-outline-success">
                             <span className="fa fa-tasks fa-1x"> </span>
-                            <span className="h5"> Mark all unread</span>
+                            <span className="h5"> Mark all as read</span>
                         </button>
                     </div>
                     <div className="list-group">
