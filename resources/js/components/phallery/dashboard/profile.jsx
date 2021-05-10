5b41f081f09189a4db80514b418c4d723e0a60d3
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-05-10 12:35:44 +0100

    styled profile section

--- resources/js/components/phallery/dashboard/profile.jsx
+++ resources/js/components/phallery/dashboard/profile.jsx
@@ -13,13 +13,81 @@ class Profile extends Component {
                         <span className="fa fa-plus-square"></span>
                         &nbsp;Logout
                     </button>
-                    <div className="container rounded shadow-lg bg-white p-3 text-center">
-                        <h1 className="fa fa-user-circle fa-5x mx-auto"></h1>
-                        <button className="btn btn-outline-info">
-                            <i className="fa fa-pencil"></i> Edit
-                        </button>
+                    <div className="container rounded shadow-lg bg-white p-3 text-center mb-3">
                         <form className="">
-                            
+                            <div className="form-group">
+                                <h1 className="mx-auto">
+                                    <span className="fa fa-user-circle fa-3x "></span>
+                                </h1>
+                                <input
+                                    type="file"
+                                    className="form-control-file"
+                                    id="photo-upload"
+                                    hidden
+                                />
+                                <label
+                                    className="btn btn-outline-success"
+                                    htmlFor="photo-upload"
+                                >
+                                    <i className="fa fa-edit"></i> Edit
+                                </label>
+                            </div>
+
+                            <div className="w-75 mx-auto">
+                                <ul class="list-group list-group-flush">
+                                    <div className="form-group ">
+                                        <li class="list-group-item border-left-0 border-right-0">
+                                            <div className="row">
+                                                <div className="col-sm-8 col-md-8">
+                                                    <input
+                                                        type="text"
+                                                        className="form-control"
+                                                    />
+                                                </div>
+                                                <div className="col-sm-4 col-md-4">
+                                                    <button className="btn btn-default">
+                                                        <span className="text-primary fa fa-pencil-alt"></span>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </li>
+
+                                        <li class="list-group-item border-left-0 border-right-0">
+                                            <div className="row">
+                                                <div className="col-sm-8 col-md-8">
+                                                    <input
+                                                        type="text"
+                                                        className="form-control"
+                                                    />
+                                                </div>
+                                                <div className="col-sm-4 col-md-4">
+                                                    <button className="btn btn-default">
+                                                        <span className="text-primary fa fa-pencil-alt"></span>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </li>
+
+                                        <li class="list-group-item border-left-0 border-right-0">
+                                            <div className="row">
+                                                <div className="col-sm-8 col-md-8">
+                                                    <input
+                                                        type="text"
+                                                        className="form-control"
+                                                        value="Test"
+                                                        disabled
+                                                    />
+                                                </div>
+                                                <div className="col-sm-4 col-md-4">
+                                                    <button className="btn btn-default">
+                                                        <span className="text-primary fa fa-pencil-alt"></span>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </div>
+                                </ul>
+                            </div>
                         </form>
                     </div>
                 </div>
