f33068d63a3dfe3f2cb3d163593a8e17bf7901b9
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-05-10 11:27:24 +0100

    styled notifications

--- resources/js/components/phallery/dashboard/notifications.jsx
+++ resources/js/components/phallery/dashboard/notifications.jsx
@@ -7,6 +7,54 @@ class Notifications extends Component {
             <React.Fragment>
                 <Nav />
                 <Banner text="Notifications" />
+
+                <div className="container my-3">
+                    <div className="text-center bg-white">
+                        <button className="btn btn-default">
+                            <span className="fa fa-tasks fa-1x"> </span>
+                            <span className="h5"> Mark all unread</span>
+                        </button>
+                    </div>
+                    <div className="list-group">
+                        <a
+                            href="#"
+                            className="list-group-item list-group-item-action flex-column align-items-start"
+                        >
+                            <small className="float-right">3 days ago</small>
+
+                            <p className="mb-1">
+                                <span className="fa fa-check"></span>&nbsp;
+                                Donec id elit non mi porta gravida at eget
+                                metus. Maecenas sed diam eget risus varius
+                                blandit.
+                            </p>
+                        </a>
+                        <a
+                            href="#"
+                            class="list-group-item list-group-item-action flex-column align-items-start"
+                        >
+                            <small className="float-right">3 days ago</small>
+                            <p className="mb-1">
+                                <span className="fa fa-check"></span>&nbsp;
+                                Donec id elit non mi porta gravida at eget
+                                metus. Maecenas sed diam eget risus varius
+                                blandit.
+                            </p>
+                        </a>
+                        <a
+                            href="#"
+                            className="list-group-item list-group-item-action flex-column align-items-start"
+                        >
+                            <small className="float-right">3 days ago</small>
+                            <p className="mb-1">
+                                <span className="text-success fa fa-check"></span>
+                                &nbsp; Donec id elit non mi porta gravida at
+                                eget metus. Maecenas sed diam eget risus varius
+                                blandit.
+                            </p>
+                        </a>
+                    </div>
+                </div>
             </React.Fragment>
         );
     }
