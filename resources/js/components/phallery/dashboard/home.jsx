83cd969b34b9a87be72b00fe5bdf5748d8a96005
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-05-12 00:40:24 +0100

    modified feeds page

--- resources/js/components/phallery/dashboard/home.jsx
+++ resources/js/components/phallery/dashboard/home.jsx
@@ -2,9 +2,7 @@ import React, { Component } from "react";
 import Nav from "./nav";
 import Banner from "./banner";
 import CreatePost from "./create_post";
-import Comment from "./post_component/comment";
-import Like from "./post_component/like";
-import Heart from "./post_component/heart";
+import PostContainer from "./post_component/postContainer";
 
 class Home extends Component {
     render() {
@@ -30,64 +28,31 @@ class Home extends Component {
                             </div>
                         </div>
                     </form>
-                    <div className="card my-3 border-0 bg-none">
-                        <img
-                            className="card-img-top col-md-6 mx-auto rounded shadow"
-                            src="../image/1a.jpg"
-                            alt="Card image cap"
+                    <div className="row">
+                        <PostContainer
+                            imgUrl="../image/1a.jpg"
+                            text="This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer."
                         />
-                        <div className="card-body col-md-6 mx-auto bg-white rounded-bottom shadow">
-                            <p className="card-text">
-                                This is a wider card with supporting text below
-                                as a natural lead-in to additional content. This
-                                content is a little bit longer.
-                            </p>
-                            <div className="card-text d-flex justify-content-around col-md-4">
-                                <Heart />
-                                <Like />
-                                <Comment />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card my-3 border-0 bg-none">
-                        <img
-                            className="card-img-top col-md-6 mx-auto rounded shadow"
-                            src="../image/1c.jpg"
-                            alt="Card image cap"
+                        <PostContainer
+                            imgUrl="../image/1a.jpg"
+                            text="This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer."
                         />
-                        <div className="card-body col-md-6 mx-auto bg-white rounded-bottom shadow">
-                            <p className="card-text">
-                                This is a wider card with supporting text below
-                                as a natural lead-in to additional content. This
-                                content is a little bit longer.
-                            </p>
-                            <div className="card-text d-flex justify-content-around col-md-4">
-                                <Heart />
-                                <Like />
-                                <Comment />
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="card my-3 border-0 bg-none">
-                        <img
-                            className="card-img-top col-md-6 mx-auto rounded shadow"
-                            src="../image/1b.jpg"
-                            alt="Card image cap"
+                        <PostContainer
+                            imgUrl="../image/1a.jpg"
+                            text="This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer."
                         />
-                        <div className="card-body col-md-6 mx-auto bg-white rounded-bottom shadow">
-                            <p className="card-text">
-                                This is a wider card with supporting text below
-                                as a natural lead-in to additional content. This
-                                content is a little bit longer.
-                            </p>
-                            <div className="card-text d-flex justify-content-around col-md-4">
-                                <Heart />
-                                <Like />
-                                <Comment />
-                            </div>
-                        </div>
+                        <PostContainer
+                            imgUrl="../image/1a.jpg"
+                            text="This is a wider card with supporting text below as a
+                        natural lead-in to additional content. This content is a
+                        little bit longer."
+                        />
                     </div>
                 </div>
             </React.Fragment>
