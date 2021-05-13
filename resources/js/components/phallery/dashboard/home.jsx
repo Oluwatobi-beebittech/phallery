0b31bf6c05dd0e3e15214a2505fde6de142e8d21
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-05-13 11:43:18 +0100

    search result UI

--- resources/js/components/phallery/dashboard/home.jsx
+++ resources/js/components/phallery/dashboard/home.jsx
@@ -26,6 +26,55 @@ class Home extends Component {
                                     </button>
                                 </div>
                             </div>
+
+                            <div className="container">
+                                <div className="search-result">
+                                    <div className="list-group">
+                                        <a
+                                            href="#"
+                                            className="list-group-item list-group-item-action d-flex flex-row align-items-center border-left-0 border-right-0"
+                                        >
+                                            <div className="img-circle-wrapper">
+                                                <img
+                                                    src="../image/1a.jpg"
+                                                    className="img-circle"
+                                                />
+                                            </div>
+                                            <p className="mb-1">
+                                                &nbsp; Alpha Juanita
+                                            </p>
+                                        </a>
+                                        <a
+                                            href="#"
+                                            className="list-group-item list-group-item-action d-flex flex-row align-items-center border-left-0 border-right-0"
+                                        >
+                                            <div className="img-circle-wrapper">
+                                                <img
+                                                    src="../image/1a.jpg"
+                                                    className="img-circle"
+                                                />
+                                            </div>
+                                            <p className="mb-1">
+                                                &nbsp; Alpha Juanita
+                                            </p>
+                                        </a>
+                                        <a
+                                            href="#"
+                                            className="list-group-item list-group-item-action d-flex flex-row align-items-center border-left-0 border-right-0"
+                                        >
+                                            <div className="img-circle-wrapper">
+                                                <img
+                                                    src="../image/1a.jpg"
+                                                    className="img-circle"
+                                                />
+                                            </div>
+                                            <p className="mb-1">
+                                                &nbsp; Alpha Juanita
+                                            </p>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </form>
                     <div className="row">
