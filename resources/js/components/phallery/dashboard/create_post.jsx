cdcebc4813cdee0c68b57290652822a006ea4ff6
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-05-14 22:34:27 +0100

    tweaked modal post creation form

--- resources/js/components/phallery/dashboard/create_post.jsx
+++ resources/js/components/phallery/dashboard/create_post.jsx
@@ -2,47 +2,100 @@ import React, { Component } from "react";
 import Modal from "react-bootstrap/Modal";
 
 class CreatePost extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { modalShow: false, postText: "", file: "",  };
+        this.displayModal = this.displayModal.bind(this);
+        this.postTextChanged = this.postTextChanged.bind(this);
+        this.onFileChanged = this.onFileChanged.bind(this);
+    }
+
+    displayModal(value) {
+        this.setState({ modalShow: value });
+    }
+
+    postTextChanged(e) {
+        this.setState({ postText: e.target.value });
+    }
+
+    onFileChanged(e) {
+        this.setState({ file: e.target.files[0] });
+    }
+
     render() {
+        const fileName =
+            this.state.file != "" ? (
+                <p>
+                    <span className="fa fa-file-image"></span>
+                    &nbsp;{this.state.file.name}
+                </p>
+            ) : (
+                ""
+            );
         return (
             <React.Fragment>
-                <button className="btn btn-outline-success my-3">
+                <button
+                    className="btn btn-outline-success my-3"
+                    onClick={() => this.displayModal(true)}
+                >
                     <span className="fa fa-plus-square"></span>
                     &nbsp;Create a post
                 </button>
 
                 <Modal
                     size="lg"
-                    show={true}
-                    onHide=""
-                    ariaLabelledBy="create-post"
+                    show={this.state.modalShow}
+                    onHide={() => this.displayModal(false)}
+                    aria-labelledby="create-post"
                     centered
                 >
                     <Modal.Header closeButton className="bg-dark text-white">
                         <Modal.Title id="create-post">
-                            <span class="fa fa-edit"></span>&nbsp;Create a post
+                            <span className="fa fa-edit"></span>&nbsp;Create a
+                            post
                         </Modal.Title>
                     </Modal.Header>
                     <Modal.Body>
                         <form>
                             <div className="form-row">
                                 <div className="form-group col-md-12">
-                                    <label htmlFor="card-title">Post</label>
-                                    <input
+                                    <label htmlFor="card-post">Post</label>
+                                    <textarea
                                         type="text"
                                         className="form-control"
-                                        id="card-title"
-                                        placeholder="Card Title"
-                                        onChange="{}"
-                                        value="{}"
-                                        maxLength={20}
-                                        ariaDescribedBy="cardTitleHelpText"
+                                        rows="3"
+                                        id="card-post"
+                                        onChange={this.postTextChanged}
+                                        value={this.state.postText}
+                                        placeholder="Write the post"
+                                        maxLength={300}
+                                        aria-describedby="cardPostHelpText"
                                     />
                                     <small
-                                        id="cardTitleHelpText"
+                                        id="cardPostHelpText"
                                         className="form-text text-muted float-right"
                                     >
-                                        {20}/20
+                                        {this.state.postText.length}/300
                                     </small>
+                                </div>
+                            </div>
+                            <div className="form-row">
+                                <div className="form-group col-md-12">
+                                    <label
+                                        htmlFor="card-upload"
+                                        className="btn btn-primary"
+                                    >
+                                        <span className="fa fa-upload"></span>
+                                        &nbsp;Upload photo
+                                    </label>
+                                    {fileName}
+                                    <input
+                                        type="file"
+                                        className="form-control"
+                                        id="card-upload"
+                                        onChange={this.onFileChanged}
+                                        hidden
+                                    />
                                 </div>
                             </div>
 
@@ -50,9 +103,8 @@ class CreatePost extends Component {
                                 <button
                                     type="submit"
                                     className="btn btn-success"
-                                    onClick="{}"
                                 >
-                                    <span class="fa fa-plus-square"></span>
+                                    <span className="fa fa-plus-square"></span>
                                     &nbsp; Create
                                 </button>
                             </div>
