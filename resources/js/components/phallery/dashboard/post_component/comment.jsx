f6f4aa517ff998a5a3e4e621fcecdde7912c4f15
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-05-15 22:32:28 +0100

    comment chat modal

--- resources/js/components/phallery/dashboard/post_component/comment.jsx
+++ resources/js/components/phallery/dashboard/post_component/comment.jsx
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
+import Modal from "react-bootstrap/Modal";
 class Comment extends Component {
     constructor(props) {
         super(props);
-        this.state = { isClicked: false, count: this.props.count };
+        this.state = {
+            isClicked: false,
+            count: this.props.count,
+            commentBoxDisplay: false,
+            commentText: ""
+        };
         this.onCommentClicked = this.onCommentClicked.bind(this);
+        this.commentTextChanged = this.commentTextChanged.bind(this);
     }
     onCommentClicked(e) {
         e.preventDefault();
-        const previousClick = this.state.isClicked;
-        const countAdjust = previousClick
-            ? this.state.count - 1
-            : this.state.count + 1;
-        this.setState({ isClicked: !previousClick, count: countAdjust });
+        this.displayCommentBox(true);
+        // const previousClick = this.state.isClicked;
+        // const countAdjust = previousClick
+        //     ? this.state.count - 1
+        //     : this.state.count + 1;
+        // this.setState({ isClicked: !previousClick, count: countAdjust });
+    }
+
+    displayCommentBox(value) {
+        this.setState({ commentBoxDisplay: value });
+    }
+
+    commentTextChanged(e) {
+        this.setState({ commentText: e.target.value });
     }
 
     render() {
@@ -27,6 +43,69 @@ class Comment extends Component {
                     className={classText}
                     onClick={this.onCommentClicked}
                 ></a>
+
+                <Modal
+                    size="md"
+                    show={this.state.commentBoxDisplay}
+                    onHide={() => this.displayCommentBox(false)}
+                    aria-labelledby="create-comment"
+                    centered
+                >
+                    <Modal.Header closeButton className="bg-dark text-white">
+                        <Modal.Title id="create-comment">
+                            <span className="fa fa-comment-dots"></span>
+                            &nbsp;Comments
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <div className="comment-box p-2 rounded-15 mb-1">
+                            <p>
+                                This is a wider card with supporting text below
+                            </p>
+                            <div className="d-flex flex-column">
+                                <div className="col-md-6 p-2 rounded-15 chat-box">
+                                    jfhf
+                                    <small>24 June 2021</small>
+                                </div>
+                                <div>
+                                    <div className="col-md-6 p-2 rounded-15 chat-box float-right">
+                                        <p className="font-weight-bold">
+                                            Oluwatobi Akanji
+                                        </p>
+                                        <p>gffj</p>
+                                        <small className="float-right font-weight-light">
+                                            24 June 2021
+                                        </small>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <form>
+                            <div className="form-row">
+                                <div className="col-md-9">
+                                    <textarea
+                                        type="text"
+                                        className="form-control"
+                                        rows="3"
+                                        id="card-post"
+                                        onChange={this.commentTextChanged}
+                                        value={this.state.commentText}
+                                        placeholder="Your comment"
+                                        maxLength={300}
+                                        aria-describedby="cardPostHelpText"
+                                    />
+                                </div>
+                                <div className=" d-flex flex-column justify-content-around text-center text-primary col-md-3">
+                                    <button className="btn btn-primary">
+                                        Send{" "}
+                                        <span className="fa fa-chevron-right"></span>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </Modal.Body>
+                </Modal>
             </React.Fragment>
         );
     }
