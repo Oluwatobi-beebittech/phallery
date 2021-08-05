2e5d23847cdf259f045b2fb79f701659415e7827
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-08-05 14:41:18 +0100

    file storage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(UrlGenerator $url)
     {
         if (env('APP_ENV') === 'production') {
-            $url->forceSchema('https');
+            $url->forceScheme('https');
         }
     }
 }
