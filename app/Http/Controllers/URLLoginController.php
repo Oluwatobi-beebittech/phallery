153e4c1cab45b5eb780ceb50db7743185303fba4
Oluwatobi-beebittech <akanjioluwatobishadrach@yahoo.com>
2021-08-06 13:43:28 +0100

    checking request object

--- app/Http/Controllers/URLLoginController.php
+++ app/Http/Controllers/URLLoginController.php
@@ -52,7 +52,7 @@ class URLLoginController extends Controller
 
 
     public function signedLogin(Request $request, $user)
-    {dd($request);
+    {print_r($request);
         if($request->hasValidSignature()){
             $user_obj = User::where('email',$user)->first();
             if($user_obj->exists()){
